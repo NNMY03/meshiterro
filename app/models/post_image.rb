ec7603f257d8ae83e4c12be2acc2896c2b4b5e3b
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-06-14 14:13:13 +0000

    [Add]データテーブルの関連付け

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,6 @@
 class PostImage < ApplicationRecord
   
-    has_one_attached :image
-  
+    has_one_attached :image #「どのモデルに対して画像を使用するのか」を指定している
+      belongs_to :user　
+    #   1:[N]側　users_id
 end
